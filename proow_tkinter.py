ed7794bd284467438f257b90ef7ca5279c62c119
EEANVOS <andres.vosa@ee.abb.com>
2021-08-09 12:59:44 +0300

    Create proow_tkinter.py

---
+++ proow_tkinter.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+def myClick():
+    myLabel3 = Label(root, text=myEntry.get())
+    myLabel3.grid() # Kui määrata kindel koht siis siin pannakse need 
+                    #labelid üksteise peale
+
+
+root = Tk()
+# creating label
+myLabel1 = Label(root, text='Hello World') #.grid(row=0, column=0)
+myLabel2 = Label(root, text='My name is Andres') #.grid(row=1, column=0)
+myButton1 = Button(root, text='Enter text', padx=50, pady=20, command=myClick, fg='red', bg='blue') #, state= DISABLED
+myEntry = Entry(root, width=50)
+# showing label 
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+myButton1.grid(row=2, column=0)
+myEntry.grid(row=2, column=1)
+# Empty columns doesn't affect layout
+# run mainloop
+root.mainloop()
+
+
