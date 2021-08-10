bb93e2029ee0174ac3e489dc1a38d26c597ffd6a
EEANVOS <andres.vosa@ee.abb.com>
2021-08-10 08:22:16 +0300

    Morning  logistic map changes 2021_08_10

--- Logistic_Map.py
+++ Logistic_Map.py
@@ -19,11 +19,13 @@ def next_lmap_no(x_n : float, r : float) -> float:
 
 @jit(nopython=True)
 def result_vector(r: float, x_0 : float = 0.5, vector_length : int = 500) -> float:
-    x = x_0 + np.zeros(vector_length)
+    x = x_0 + np.zeros(vector_length, dtype=np.float32)
     
     for i in range(vector_length - 1):
         x[i + 1] = next_lmap_no(x[i], r)
     return x
+
+
 
 def main():
     print(next_lmap_no(2, 2))
@@ -33,6 +35,9 @@ def main():
     plt.plot(x_plot, 's-')
     plt.grid()
     plt.show()
+    
+    r_arr = np.linspace(0.1, 4.0, 1000, dtype=float)
+    print(r_arr)
     
     
 
