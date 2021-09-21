e26951926f563afea1db29d96d2722e6dafd1740
EEANVOS <andres.vosa@ee.abb.com>
2021-09-21 10:42:41 +0300

    -

--- DPGui_Test.py
+++ DPGui_Test.py
@@ -3,7 +3,7 @@ import dearpygui.dearpygui as dpg
 def save_callback():
     print("Save Clicked")
 
-with dpg.window(label="Example Window"):
+with dpg.window(label="Example Window", height=300, width=300):
     dpg.add_text("Hello world")
     dpg.add_button(label="Save", callback=save_callback)
     dpg.add_input_text(label="string")
