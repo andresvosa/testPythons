cdfbbdf2b952e116982d8509cecab6824200ad21
EEANVOS <andres.vosa@ee.abb.com>
2021-09-24 11:59:08 +0300

    2021_09_24 reviewed

--- proow_tkinter.py
+++ proow_tkinter.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
-def myClick():
-    myLabel3 = Label(root, text=myEntry.get())
-    myLabel3.grid() # Kui määrata kindel koht siis siin pannakse need 
+def my_click():
+    my_label_3 = Label(root, text=myEntry.get())
+    my_label_3.grid() # Kui määrata kindel koht siis siin pannakse need 
                     #labelid üksteise peale
 
 
@@ -10,7 +10,7 @@ root = Tk()
 # creating label
 myLabel1 = Label(root, text='Hello World') #.grid(row=0, column=0)
 myLabel2 = Label(root, text='My name is Andres') #.grid(row=1, column=0)
-myButton1 = Button(root, text='Enter text', padx=50, pady=20, command=myClick, fg='red', bg='blue') #, state= DISABLED
+myButton1 = Button(root, text='Enter text', padx=50, pady=20, command=my_click, fg='red', bg='blue') #, state= DISABLED
 myEntry = Entry(root, width=50)
 # showing label 
 myLabel1.grid(row=0, column=0)
