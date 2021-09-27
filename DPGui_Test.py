a07a8eba8b9ebdfacc3c51bdc11e63d03acb7074
EEANVOS <andres.vosa@ee.abb.com>
2021-09-27 15:17:52 +0300

    2021_09_27 Values from dpg

--- DPGui_Test.py
+++ DPGui_Test.py
@@ -1,13 +1,16 @@
 import dearpygui.dearpygui as dpg
 
 def save_callback():
-    print("Save Clicked")
+    print("Save Clicked: " + dpg.get_value(sis_tekst))
 
 with dpg.window(label="Example Window", height=300, width=300):
     dpg.add_text("Hello world")
     dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="string")
+    sis_tekst = dpg.add_input_text(label="string")
     dpg.add_slider_float(label="float")
 
+
 dpg.setup_viewport()
+dpg.set_viewport_height(300)
+dpg.set_viewport_width(300)
 dpg.start_dearpygui()
